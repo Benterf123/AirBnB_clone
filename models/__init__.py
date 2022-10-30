9d75b49d7fa72d701d622e692d7564d1b5277800
Benterf123 <faithbenter@gmail.com>
2022-10-30 14:46:14 -0700

    Hot fix

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+"""Creates a package for the models module."""
+
+from models.engine.file_storage import FileStorage
+
+"""This module contains the storage instance."""
+
+storage = FileStorage()
+
+storage.reload()
